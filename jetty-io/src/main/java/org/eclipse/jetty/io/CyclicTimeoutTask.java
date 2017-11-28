d227a4d2a15b654fd435c51487047861a6d64ab4
Greg Wilkins <gregw@webtide.com>
2017-11-28 09:30:43 +0100

    Blocking CyclicTimeoutTask implementation
    Signed-off-by: Greg Wilkins <gregw@webtide.com>

--- jetty-io/src/main/java/org/eclipse/jetty/io/CyclicTimeoutTask.java
+++ jetty-io/src/main/java/org/eclipse/jetty/io/CyclicTimeoutTask.java
@@ -21,8 +21,6 @@ package org.eclipse.jetty.io;
 import static java.lang.Long.MAX_VALUE;
 
 import java.util.concurrent.TimeUnit;
-import java.util.concurrent.atomic.AtomicLong;
-import java.util.concurrent.atomic.AtomicReference;
 
 import org.eclipse.jetty.util.thread.Scheduler;
 
@@ -38,18 +36,14 @@ public abstract class CyclicTimeoutTask
     private final Scheduler _scheduler;
 
     /* 
-     * The time at which this task will expire (or MAX_VALUE if it will never expire.
-     * This is the primary concurrent mechanism in this class and must be updated
-     * correctly with looping compare and sets.
+     * The time at which this task will expire (or MAX_VALUE if it will never expire).
      */
-    private final AtomicLong _expireAtNanos = new AtomicLong(Long.MAX_VALUE);
+    private long _expireAtNanos = MAX_VALUE;
     
     /*
-     * A reference to a scheduled expiry of the underlying scheduler.  This is kept
-     * as an atomic reference, but it is only held as an optimization, so it is not
-     * important if updates to _scheduled are lost. 
+     * A reference to a scheduled expiry of the underlying scheduler chain. 
      */
-    private final AtomicReference<Scheduled> _scheduled = new AtomicReference<>();
+    private Scheduled _scheduled ;
     
 
     /**
@@ -73,28 +67,16 @@ public abstract class CyclicTimeoutTask
      */
     public void schedule(long delay, TimeUnit units) throws IllegalStateException
     {
-        // Calculate the nanotime at which we will expire
-        long now = System.nanoTime();
-        long expireAtNanos = now + units.toNanos(delay);
-    
-        // Update the atomic. If we fail, then timer was already set.
-        if (!_expireAtNanos.compareAndSet(MAX_VALUE,expireAtNanos))
-            throw new IllegalStateException("Timeout pending");
-    
-        // We've updated the expiry time, so we need a schedule to service that time.
-        // Let's look if there is one already set that is good enough?
-        Scheduled scheduled = _scheduled.get();
-        if (scheduled==null || scheduled._scheduledAt>expireAtNanos)
+        synchronized(this)
         {
-            // Either there is no schedule expiry, or we can see one that expires after we
-            // need it. So we need our own scheduled expiry. 
-            Scheduled rescheduled = new Scheduled(now,expireAtNanos);
+            if (_expireAtNanos!=MAX_VALUE)
+                throw new IllegalStateException("Timeout pending");
             
-            // Now that we have our own schedule, let's try to set it so it can be reused.
-            // If we fail to set it, that's no problem as we are still scheduled. The only
-            // down side is that subsequent calls to schedule or reschedule may create an
-            // extra unnecessary schedule.
-            _scheduled.compareAndSet(scheduled,rescheduled);
+            // Calculate the nanotime at which we will expire
+            long now = System.nanoTime();
+            _expireAtNanos = now + units.toNanos(delay);
+
+            schedule(now);
         }
     }
     
@@ -105,49 +87,53 @@ public abstract class CyclicTimeoutTask
      * @return True if the timer was already set.
      */
     public boolean reschedule(long delay, TimeUnit units)
-    {
-        // Calculate the nanotime at which we will expire
-        long now = System.nanoTime();
-        long expireAtNanos = now + units.toNanos(delay);
-    
-        // loop until we manage to update the expiry time
-        while(true)
+    {   
+        boolean was_scheduled;
+        synchronized(this)
         {
-            long expireAt = _expireAtNanos.get();
-            if (_expireAtNanos.compareAndSet(expireAt,expireAtNanos))
-            {        
-                // We've updated the expiry time, so we need a schedule to service that time.
-                // Let's look if there is one already set that is good enough?
-                Scheduled scheduled = _scheduled.get();
-                if (scheduled==null || scheduled._scheduledAt>expireAtNanos)
-                {
-                    // The schedule we can see is not good enough (either null or 
-                    // expires too late), so make our own. 
-                    Scheduled rescheduled = new Scheduled(now,expireAtNanos);
-                    
-                    // We have scheduled our own timeout, so it would be good to make it 
-                    // visible to other threads that may call reschedule.  But if this fails 
-                    // it is not a problem as they will create their own Schedule
-                    _scheduled.compareAndSet(scheduled,rescheduled);
-                }
-                return expireAt!=MAX_VALUE;
-            }
+            was_scheduled = _expireAtNanos!=MAX_VALUE;
+
+            // Calculate the nanotime at which we will expire
+            long now = System.nanoTime();
+            _expireAtNanos = now + units.toNanos(delay);
+
+            schedule(now);
         }
+        return was_scheduled;
+    }
+    
+    private void schedule(long now)
+    {
+        if (_scheduled==null || _scheduled._scheduledAt>_expireAtNanos)
+            _scheduled = new Scheduled(now,_expireAtNanos,_scheduled);
     }
    
     public boolean cancel()
     {
-        return _expireAtNanos.getAndSet(MAX_VALUE)!=MAX_VALUE;
+        boolean was_scheduled;
+        synchronized(this)
+        {
+            was_scheduled = _expireAtNanos!=MAX_VALUE;
+            _expireAtNanos = MAX_VALUE;
+        }
+        return was_scheduled;
     }
     
     public abstract void onTimeoutExpired();
 
     public void destroy()
     {
-        cancel();
-        Scheduled scheduled = _scheduled.getAndSet(null);
-        if (scheduled!=null)
-            scheduled._task.cancel();
+        synchronized(this)
+        {
+            _expireAtNanos = MAX_VALUE;
+
+            Scheduled scheduled = _scheduled;
+            while (scheduled!=null)
+            {
+                scheduled._task.cancel();
+                scheduled = scheduled._chain;
+            }
+        }
     }
     
     /**
@@ -157,64 +143,57 @@ public abstract class CyclicTimeoutTask
     {
         final long _scheduledAt;
         final Scheduler.Task _task;
+        final Scheduled _chain;
         
-        Scheduled(long now, long scheduledAt)
+        Scheduled(long now, long scheduledAt, Scheduled chain)
         {
             _scheduledAt = scheduledAt;
             _task = _scheduler.schedule(this,scheduledAt-now,TimeUnit.NANOSECONDS);
+            _chain = chain;
         }
         
         @Override
         public void run()
         {
-            // Our scheduled real timer has expired, so we must check if our real
-            // expiry has passed.  We do this in a loop as we must atomically compare
-            // and set to indicate the timer has expired.
-            while(true)
+            boolean expired = false;
+            synchronized (CyclicTimeoutTask.this)
             {
                 long now = System.nanoTime();
-                long expireAt = _expireAtNanos.get();
-                
-                // If we expireAt MAX_VALUE, we are cancelled, so return without making a new Scheduled
-                if (expireAt==MAX_VALUE)
-                {   
-                    // We've expired, so matter what we will not be expiring again.
-                    // So remove ourselves form the scheduled reference.  Doesn't matter
-                    // if we fail as it indicates we've already been replaced.
-                    _scheduled.compareAndSet(this,null);
-                    
-                    return;
-                }
-
-                // Are we actually expired?
-                if (expireAt<now)
+             
+                // Remove ourselves from the chain (and any strangely unexpired prior Scheduled)
+                Scheduled scheduled = _scheduled;
+                while (scheduled!=null)
                 {
-                    // Update with CAS so that only one Scheduled will actually expire the
-                    // timeout (as there may be multiple Scheduled instances for similar times).
-                    if (!_expireAtNanos.compareAndSet(expireAt,MAX_VALUE))
-                        continue; // Somebody beat us to it, so retry
-                    
-                    // We've expired, so matter what we will not be expiring again.
-                    // So remove ourselves form the scheduled reference.  Doesn't matter
-                    // if we fail as it indicates we've already been replaced.
-                    _scheduled.compareAndSet(this,null);
-                    
-                    // Let's tell our derived selfs that we have timed out!
-                    onTimeoutExpired();
-                    return;
+                    Scheduled last = scheduled;
+                    scheduled = scheduled._chain;
+                    if (last==this)
+                        break;
                 }
+                _scheduled = scheduled;
                 
-                // Our scheduled expiry was too early for the current expiry, so
-                // we need to create a new Scheduled for our actual expiry
-                Scheduled scheduled = new Scheduled(now,expireAt);
                 
-                // We have created a new Scheduled, so our current expiry is covered.
-                // As an optimisation, we set this on the atomic so it can be seen and
-                // used by other calls to schedule.  If we fail, that is not a problem 
-                // as it just means a redundant Scheduled will be created.
-                _scheduled.compareAndSet(this,scheduled);
+                if (now>=_expireAtNanos)
+                {
+                    expired = true;
+                    _expireAtNanos = MAX_VALUE;
+                }
+                else if (now!=MAX_VALUE)
+                {
+                    schedule(now);
+                }                    
             }
+            
+            if (expired)
+                onTimeoutExpired();
+            
         }
+        
+        @Override
+        public String toString()
+        {
+            return String.format("Scheduled@%x:%d",hashCode(),_expireAtNanos);
+        }
+        
     }
     
 }
